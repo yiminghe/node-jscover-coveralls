6fc04118a7d34d146a4eacf827076c7de98e301a
yiminghe <yiminghe@gmail.com>
2014-08-12 21:20:23 +0800

    update

--- index.js
+++ index.js
@@ -4,33 +4,37 @@ var path = require('path');
 var jobId = process.env.TRAVIS_JOB_ID;
 module.exports = function (config) {
     return function (req, res, next) {
-        if (jobId && req.path === '/node-jscover-coveralls') {
-            var postData = {
-                'service_name': 'travis-ci',
-                'service_job_id': jobId,
-                'sourceFiles': []
-            };
-            var report = req.param('report');
-            console.log(report);
-            var jsonReport = JSON.parse(report);
-            var sourceFiles = postData.sourceFiles;
-            for (var f in jsonReport) {
-                var detail = jsonReport[f];
-                // coveralls.io does not need first data
-                detail.lineData.shift();
-                var name = f;
-                var source = fs.readFileSync(path.join(config.base, f), 'utf8');
-                sourceFiles.push({
-                    name: name,
-                    source: source,
-                    coverage: detail.lineData
+        if (req.path === '/node-jscover-coveralls') {
+            if (jobId) {
+                var postData = {
+                    'service_name': 'travis-ci',
+                    'service_job_id': jobId,
+                    'sourceFiles': []
+                };
+                var report = req.param('report');
+                console.log(report);
+                var jsonReport = JSON.parse(report);
+                var sourceFiles = postData.sourceFiles;
+                for (var f in jsonReport) {
+                    var detail = jsonReport[f];
+                    // coveralls.io does not need first data
+                    detail.lineData.shift();
+                    var name = f;
+                    var source = fs.readFileSync(path.join(config.base, f), 'utf8');
+                    sourceFiles.push({
+                        name: name,
+                        source: source,
+                        coverage: detail.lineData
+                    });
+                }
+                var str = JSON.stringify(postData);
+                var url = 'https://coveralls.io/api/v1/jobs';
+                request.post({url: url, form: { json: str}}, function () {
+                    res.send('ok');
                 });
+            } else {
+                res.send('no effect');
             }
-            var str = JSON.stringify(postData);
-            var url = 'https://coveralls.io/api/v1/jobs';
-            request.post({url: url, form: { json: str}}, function () {
-                res.send('ok');
-            });
         } else {
             next();
         }
