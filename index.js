8f709f4effabf596e784cc2810ecef2ea64f9d12
yiminghe <yiminghe@gmail.com>
2014-08-13 12:47:18 +0800

    update travis

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var coveralls = require('coveralls');
 module.exports = function () {
     return function (req, res, next) {
         if (req.path === '/node-jscover-coveralls') {
+            if (!process.env.TRAVIS_JOB_ID) {
+                res.end('no effect');
+                return;
+            }
             var lcov = req.param('lcov');
             coveralls.handleInput(lcov, function (err) {
                 console.log('coveralls:', err || 'ok');
